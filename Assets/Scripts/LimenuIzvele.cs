28ee2454b5acb03d861eb9e7d931333eae25487d
2PT-2 <118617121+zieduvija@users.noreply.github.com>
2023-04-18 12:15:15 +0300

    Update3
    Pievienoti ļoti bēdīgi putni un līmeņu izvēle

---
+++ Assets/Scripts/LimenuIzvele.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LimenuIzvele : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public TextMeshProUGUI vietasNosaukums;
+    public TextMeshProUGUI vietasApraksts;
+    public GameObject bildesPanelis;
+    Sprite pirmais,otrais;
+
+
+    public void pirmaisLimenis() {
+        vietasNosaukums.text = "Liepājas pludmale";
+        vietasApraksts.text = "Pludmale Liepājas krastā.\nUzdevums: Attīrīt no atkritumiem!";
+        bildesPanelis.gameObject.GetComponent<Image>().sprite = pirmais;
+    }
+
+    public void otraisLimenis()
+    {
+        vietasNosaukums.text = "Dienvidu forti";
+        vietasApraksts.text = "Forti Liepājas dienvidos.\nUzdevums: Attīrīt no atkritumiem!";
+        bildesPanelis.gameObject.GetComponent<Image>().sprite = otrais;
+    }
+
+}
